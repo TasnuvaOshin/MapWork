4f1df1144f1bfc4ad5381c5cfb2adb4a164babe0
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-07-21 12:16:32 +0600

    My Commit For Auto Complete Search Box

---
+++ app/src/main/java/com/khudrosoft/myapplication/TestMapActivity.java
@@ -0,0 +1,14 @@
+package com.khudrosoft.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class TestMapActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test_map);
+    }
+}
