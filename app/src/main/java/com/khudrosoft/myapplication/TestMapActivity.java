0d467b6ac19c14ee6ad122f3ba9db53292812961
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-07-22 11:53:44 +0600

    Auto Complete Works

--- app/src/main/java/com/khudrosoft/myapplication/TestMapActivity.java
+++ app/src/main/java/com/khudrosoft/myapplication/TestMapActivity.java
@@ -1,14 +1,57 @@
 package com.khudrosoft.myapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.google.android.gms.common.api.Status;
+import com.google.android.gms.location.places.Place;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+
+import java.util.Arrays;
+
 public class TestMapActivity extends AppCompatActivity {
+    private String Api_key;
+    private PlacesClient placesClient;
+    private AutocompleteSupportFragment autocompleteSupportFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_test_map);
+        Api_key = "AIzaSyAhje3XitH37m7iMYmVh0U1hnzsGmCLaSI";
+
+        if (!Places.isInitialized()) {
+            Places.initialize(getApplicationContext(), Api_key);
+
+        }
+        placesClient = Places.createClient(this);
+        autocompleteSupportFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.f_autocomplete);
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(com.google.android.libraries.places.api.model.Place.Field.ID, com.google.android.libraries.places.api.model.Place.Field.NAME));
+
+        autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(@NonNull com.google.android.libraries.places.api.model.Place place) {
+                final LatLng latLng = place.getLatLng();
+
+
+
+
+
+            }
+
+            @Override
+            public void onError(@NonNull Status status) {
+
+            }
+        });
+
+
+
     }
 }
