91b5e58a02e99cbd9283af1ccc4a47406342287a
Tasnuva Tabassum Oshin <tasnuva.oshin12@gmail.com>
2019-07-23 17:59:30 +0600

    Auto Complete Works

--- app/src/main/java/com/khudrosoft/myapplication/TestMapActivity.java
+++ app/src/main/java/com/khudrosoft/myapplication/TestMapActivity.java
@@ -1,24 +1,54 @@
 package com.khudrosoft.myapplication;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
 
 import com.google.android.gms.common.api.Status;
-import com.google.android.gms.location.places.Place;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 
 import java.util.Arrays;
 
-public class TestMapActivity extends AppCompatActivity {
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+
+import static com.khudrosoft.myapplication.MyMap.MapActivity.MY_PERMISSIONS_REQUEST_LOCATION;
+
+public class TestMapActivity extends AppCompatActivity implements OnMapReadyCallback {
     private String Api_key;
     private PlacesClient placesClient;
     private AutocompleteSupportFragment autocompleteSupportFragment;
+    private FusedLocationProviderClient fusedLocationProviderClient;
+    private static final float DEFAULT_ZOOM = 15f;
+   private String provider;
+   private Location location;
+    //now for showing the map
+
+    private GoogleMap mMap;
+    private SupportMapFragment mapFragment;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,26 +62,147 @@ public class TestMapActivity extends AppCompatActivity {
         }
         placesClient = Places.createClient(this);
         autocompleteSupportFragment = (AutocompleteSupportFragment) getSupportFragmentManager().findFragmentById(R.id.f_autocomplete);
-        autocompleteSupportFragment.setPlaceFields(Arrays.asList(com.google.android.libraries.places.api.model.Place.Field.ID, com.google.android.libraries.places.api.model.Place.Field.NAME));
+        autocompleteSupportFragment.setPlaceFields(Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG));
 
         autocompleteSupportFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
             @Override
-            public void onPlaceSelected(@NonNull com.google.android.libraries.places.api.model.Place place) {
-                final LatLng latLng = place.getLatLng();
+            public void onPlaceSelected(Place place) {
+                // TODO: Get info about the selected place.
+              //  txtView.setText(place.getName()+"\n"+place.getId()+"\n"+ place.getLatLng());
+                Log.i("loc", "Place: " + place.getName() + ", " +  place.getLatLng());
 
 
-
-
-
+                UpdateCamera(place.getLatLng(),DEFAULT_ZOOM,place.getName());
             }
 
             @Override
-            public void onError(@NonNull Status status) {
-
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("loc", "An error occurred: " + status);
             }
         });
 
 
+
+
+
+
+//map work
+        mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+
+        if(checkLocationPermission()){
+
+            mapFragment.getMapAsync(this);
+
+
+
+
+
+
+        }
+
+
+    }
+
+
+    //checking the Location Permission for map
+
+
+    public boolean checkLocationPermission() {
+        if (ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+                new AlertDialog.Builder(this)
+                        .setTitle("Permission")
+                        .setMessage("Please Share/on Your Location")
+                        .setPositiveButton("ok", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialogInterface, int i) {
+                                //Prompt the user once explanation has been shown
+                                ActivityCompat.requestPermissions(TestMapActivity.this,
+                                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                                        MY_PERMISSIONS_REQUEST_LOCATION);
+                            }
+                        })
+                        .create()
+                        .show();
+
+
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        MY_PERMISSIONS_REQUEST_LOCATION);
+            }
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    @SuppressLint("NewApi")
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+
+        if (checkLocationPermission()) {
+            mMap = googleMap;
+            GetMyLocation();
+            if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+        }
+
+
+
+    }
+
+    private void GetMyLocation() {
+
+        //checking my current Location
+
+        if (checkLocationPermission()) {
+            Log.d("debug", "Now We want to show our current location");
+            fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this);
+            @SuppressLint("MissingPermission") Task LocationTask = fusedLocationProviderClient.getLastLocation();
+
+            LocationTask.addOnCompleteListener(new OnCompleteListener() {
+
+                @Override
+                public void onComplete(@NonNull Task task) {
+                    if (task.isSuccessful()) {
+
+                        Log.d("debug", "Now We get our Current Location");
+
+                        location = (Location) task.getResult();
+                        UpdateCamera(new LatLng(location.getLatitude(), location.getLongitude()), DEFAULT_ZOOM, "Location");
+
+                    }
+                }
+            });
+        }
+
+
+    }
+
+    private void UpdateCamera(LatLng latLng, float zoom, String title) {
+        if (checkLocationPermission()) {
+            Log.d("debug", "We will now move the camera for current place");
+
+            mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+            MarkerOptions markerOptions = new MarkerOptions().position(latLng).title(title);
+            mMap.addMarker(markerOptions);
+        }
 
     }
 }
